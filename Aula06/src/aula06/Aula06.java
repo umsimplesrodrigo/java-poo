2287906c77da278c3ea5c60c49be69ccc81921b5
umsimplesrodrigo <umsimplesti@gmail.com>
2025-03-09 02:43:33 -0300

    Aula 6 - Encapsulamento

---
+++ Aula06/src/aula06/Aula06.java
@@ -0,0 +1,17 @@
+package aula06;
+
+/**
+ *
+ * @author umsimplesfdp
+ */
+public class Aula06 {
+
+    public static void main(String[] args) {
+        ControleRemoto c1 = new ControleRemoto();
+        
+        c1.ligar();
+        c1.abrirMenu();
+        
+    }
+    
+}
